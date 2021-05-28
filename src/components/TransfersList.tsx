6c983f7519b2a7e28e0b44b9cf0b43b2c718bfa0
Gacek <gzgk@gft.com>
2021-05-29 00:20:11 +0200

    add fetching but link is wrong

---
+++ src/components/TransfersList.tsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import * as data from "./../API/fakeAPI_params.json";
+//import TransferListItem from './TransferListItem';
+
+
+const TransferList = () => {
+
+    const [listOfItems, setListOfItems] = useState([]);
+
+    useEffect(() => {
+        fetch("./../API/fakeAPI_params.json", {
+            headers : { 
+              'Content-Type': 'application/json',
+              'Accept': 'application/json'
+            }}).then((res) => {
+                return res;}).then((data) => {
+                    return data.json()}).then((jsonElement) => {
+                        console.log(jsonElement)})
+    }, [])
+
+    return (
+        <div>
+            <h1>NUMBER OF TRANSFERS</h1>
+            {listOfItems.map((x) => {return x})}
+        </div>
+    );
+} 
+export default TransferList;
